bd29cb7b449cf3d297da97e03ef5250e93af2f76
Your username <your email>
2020-09-22 04:10:08 +0000

    gh-pages

---
+++ precache-manifest.a88d5ebb504105fea0e9cf4acae4c7e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41359fd2ae0975f4c367",
+    "url": "/skaktesktate/css/app.5de52152.css"
+  },
+  {
+    "revision": "7b45b2f32c4caf336967ce954d5793bb",
+    "url": "/skaktesktate/img/tudo.7b45b2f3.gif"
+  },
+  {
+    "revision": "31404262d4e0f87b98aa9348ef65130a",
+    "url": "/skaktesktate/index.html"
+  },
+  {
+    "revision": "41359fd2ae0975f4c367",
+    "url": "/skaktesktate/js/app.f62f58cc.js"
+  },
+  {
+    "revision": "9d752fd5dee9ef7e5092",
+    "url": "/skaktesktate/js/chunk-vendors.7b5ac5b3.js"
+  },
+  {
+    "revision": "2566e36add336e9d759170333031185e",
+    "url": "/skaktesktate/manifest.json"
+  },
+  {
+    "revision": "3020b204db59a166f116051010d6253f",
+    "url": "/skaktesktate/media/daasdsdd.3020b204.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/skaktesktate/robots.txt"
+  }
+]);
