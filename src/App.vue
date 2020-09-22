bb901b6dfafc41ea903c498f51d58990ec2bd986
Alex Aguiar <alex3aguiar@gmail.com>
2020-09-22 00:17:41 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+   
+    <img   v-if="step == 0" src="./assets/skate/dasdasd1.png">
+    <img  v-if="step == 1" src="./assets/skate/andando.gif">
+
+    <img   v-if="step == 2" src="./assets/skate/tudo.gif">
+
+
+  </div>
+  <div>
+  <button v-if="step == 1"   v-on:click="step = 0"> parar</button>
+  <button v-if="step == 0"   v-on:click="step = 1">remar</button>
+  <button   v-if="step != 2" :disabled="step == 0" v-on:click="step = 2">se empolgar</button>
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data:() => ({
+    step: 0
+  })
 }
 </script>
 
@@ -24,5 +35,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img{
+  width: 200px
+}
+html{
+  background-color: black
 }
 </style>
